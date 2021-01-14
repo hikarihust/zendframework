2f9d6732d102758cd66517c0f02660fbcc1078d7
vudinhquang <vudinhquangk53@gmail.com>
2021-01-14 23:59:10 +0900

    Xây dựng Module Training - Part 1

---
+++ basic/module/Training/config/module.config.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Training;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'training' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/training[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
